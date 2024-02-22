ec0559110a9ed0cd03727f6552180e705e853cbc
Sundar Murugesan <sundarm14051999@gmail.com>
2024-02-22 11:40:11 +0000

    commit

--- models/example/ephemeral.sql
+++ models/example/ephemeral.sql
@@ -1,3 +1,5 @@
 {{ config(materialized='ephemeral') }}
 
- select * from {{ source('SNOWFLAKE', 'customer') }}
+with teams as ( select * from {{ source('SNOWFLAKE', 'customer') }})
+
+select * from teams
