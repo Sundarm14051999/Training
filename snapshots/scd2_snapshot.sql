daa8eed2211842d9aa96147f5c8b1d635cd894d7
Sundar Murugesan <sundarm14051999@gmail.com>
2024-02-22 05:52:12 +0000

    commit

--- snapshots/scd2_snapshot.sql
+++ snapshots/scd2_snapshot.sql
@@ -1,13 +1,14 @@
-{% snapshot customer_snaps %}
+{% snapshot shipping_snap %}
     {{
         config(
-          target_database='raw',
-           target_schema='DBT_SMURUGESAN',
-          unique_key='id',
-          strategy='timestamp',
-          updated_at='updated_at'
+            target_database="RAW",
+            target_schema="DBT_SMURUGESAN",
+            unique_key="id",
+            strategy="timestamp",
+            updated_at="updated_at",
         )
     }}
     -- Pro-Tip: Use sources in snapshots!
-select * from {{ source('snapshot', 'shipping')  }}
+    select *
+    from {{ source("snapshot", "shipping") }}
 {% endsnapshot %}
