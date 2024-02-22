0c40d6e0f38fb9b3627461d6505d54b3c4547179
Sundar Murugesan <sundarm14051999@gmail.com>
2024-02-22 06:35:35 +0000

    snapshot

---
+++ snapshots/column_check_snapshot.sql
@@ -0,0 +1,14 @@
+{% snapshot shipping_cols %}
+    {{
+        config(
+            target_database="RAW",
+            target_schema="DBT_SMURUGESAN",
+            unique_key="ID",
+            strategy="check",
+            check_cols=['name','city','friend_name'],
+        )
+    }}
+    -- Pro-Tip: Use sources in snapshots!
+    select id,name,city
+    from {{ source("snapshot", "shipping") }}
+{% endsnapshot %}
